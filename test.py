df45bc170e30c27556e71639252fd7c32ec0021a
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2024-09-18 20:02:32 +0600

    learn python

---
+++ test.py
@@ -0,0 +1,10 @@
+# Assigning values to variables
+a = 2
+b = 3
+
+sum = a + b
+print("The sum is:", sum)
+
+# Another way to calculate and print the sum in a single line(Multiple statements on one line using semicolons (not commonly used in Python))
+
+a = 2; b = 3; sum = a + b; print("The sum is:",sum)
