47389551792bc9b843445d4767539afd03d5b1c0
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2024-09-22 00:15:23 +0600

    practice a task

---
+++ practice.py
@@ -0,0 +1,21 @@
+# Write the Python code of a program that reads two numbers from the user, and prints their sum product, and difference.
+#Hint: subtract the second number from the first one =======================================================Example 1:
+# Sample Input: 4 and 5
+# Sample Output:
+#Sum = 9
+#product = 20
+#Difference = -1
+
+num_1 = 4
+num_2 = 5
+
+sumResult = num_1 + num_2
+
+sumProductResult = num_1 * num_2
+
+productDefference = num_1 - num_2
+
+
+print("Sum Result : ", sumResult  )
+print("Sum Product Result : ", sumProductResult)
+print("Product Defference : ", productDefference)
