cba082902bb2dc92740927014f7f904cca408dc7
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2024-09-23 22:47:20 +0600

    Implement integer multiple checker for 2 and 5

--- practice.py
+++ practice.py
@@ -107,3 +107,27 @@ else:
     print("The number is Odd")
 
 
+'''
+5.
+Write the Python code of a program that reads an integer as input from the user, and prints the integer
+if it is a multiple of 2 OR 5 and prints "Not a multiple of 2 OR 5" otherwise.
+For example, 2, 4, 5, 6, 8, 10, 12, 14, 15, 16, 18, 20, 22 … i.e. this includes multiples of 2 only,
+multiples of 5 only and multiples of 2 and 5 both.
+===
+hint(1): we may use the modulus (%) operator for checking the divisibility
+hint(2): we can consider the number to be an integer
+===
+Sample Input 1:
+5
+Sample Output 1:
+5
+'''
+
+# Get integer from the user
+num = 5
+
+# Check if the number is a multiple of 2 or 5
+if num % 2 == 0 or num % 5 == 0:
+    print(num)
+else:
+    print("Not a multiple of 2 OR 5")
