1040d09aa3f04594288dcdb971d8e5c910a0092b
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2024-09-23 22:22:00 +0600

    Refactor even/odd checker output for consistency in capitalization

--- practice.py
+++ practice.py
@@ -1,4 +1,4 @@
-# Write the Python code of a program that reads two numbers from the user, and prints their sum product, and difference.
+#1. Write the Python code of a program that reads two numbers from the user, and prints their sum product, and difference.
 #Hint: subtract the second number from the first one =======================================================Example 1:
 # Sample Input: 4 and 5
 # Sample Output:
@@ -25,6 +25,7 @@ print("Product Defference : ", productDefference)
 
 
 '''
+2.
 Write the Python code of a program that reads two numbers from the user. The program should then
 print "First is greater" if the first number is greater, "Second is greater" if the second number is
 greater, and "The numbers are equal" otherwise.
@@ -50,6 +51,7 @@ else:
     
     
 '''
+3.
 Write the Python code of a program that reads two numbers, subtracts the smaller number from the
 larger one, and prints the result.
 Hint: First, we may check which number is greater
@@ -62,10 +64,11 @@ Sample Output 1:
 Explanation: -4 > -40 so -4 - (-40) = -4 + 40 = 36
 '''
 
-#Check which number is larger, then subtract the smaller from the larger
+#Sample Input:
 a = -40
 b = -4
 
+#Check which number is larger, then subtract the smaller from the larger
 
 if a > b:
      difference = a - b
@@ -73,3 +76,34 @@ else:
     difference = b - a
     
 print("The difference is :", difference)
+
+
+'''
+4.
+Write the Python code of a program that reads a number, and prints "The number is even" or "The
+number is odd", depending on whether the number is even or odd.
+hint(1): we may use the modulus (%) operator to check for even or odd
+hint(2): we can consider the number to be an integer.
+
+Sample Input 1:
+7
+Sample Output 1:
+The number is odd
+
+Sample Input 2:
+10
+Sample Output 2:
+The number is even
+
+'''
+
+#Sample Input number
+number = 7
+
+# Check if the number is Even or Odd.
+if number % 2 == 0 :
+    print("The number is Even")
+else:
+    print("The number is Odd")
+
+
